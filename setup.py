be0235a14d43068f4c3023af470f76a5e4b64c5e
Emre Hasegeli <hasegeli@tart.com.tr>
2013-02-08 21:15:47 +0200

    Setup.py added.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup (name = 'motop',
+        version = '1.0',
+        author = 'Emre Hasegeli',
+        author_email = 'hasegeli@tart.com.tr',
+        scripts = ['motop.py'],
+        license = 'ICS',
+        url = 'https://github.com/tart/motop',
+        description = 'Realtime monitoring tool for several MongoDB servers.',
+        keywords = 'mongo realtime monitoring examine explain kill operations',
+        install_requires = ['pymongo'])
+
